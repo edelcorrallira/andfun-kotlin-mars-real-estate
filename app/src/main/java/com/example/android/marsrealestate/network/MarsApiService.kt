4e8a7f02d56067eafefa26769c0a458752788cd9
Eduardo <acorral@arkusnexus.com>
2019-08-19 17:52:32 -0700

    Implemented retrofit API service with mochi JSON parser

--- app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -17,4 +17,45 @@
 
 package com.example.android.marsrealestate.network
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+
 private const val BASE_URL = "https://mars.udacity.com/"
+
+
+//Moshi (JSON interpreter used with Retrofit)
+private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
+//Factory API builder call, generates API
+private val retrofit = Retrofit.Builder()
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .baseUrl(BASE_URL)
+        .build()
+
+
+/**
+ * Defines the API Retrofit will create
+ */
+interface MarsApiService{
+    /**
+     * Retrieve property items, the annotation allows Retrofit to build the functionality
+     */
+    @GET("realestate")//Endpoint
+    fun getProperties():
+            //The call object is used to start the request
+            Call<List<MarsProperty>>
+}
+
+//Since Retrofit services are expensive and we only use one in this app, we expose it to the whole
+// app
+object MarsApi {
+    val retrofitService: MarsApiService by lazy {
+        retrofit.create(MarsApiService::class.java)
+    }
+}
